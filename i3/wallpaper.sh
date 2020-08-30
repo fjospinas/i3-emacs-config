5c3325aeb9884da7255035a5481a74f7bc01d7b8
GerrySoft <gerrysoft@outlook.es>
2020-08-31 00:20:30 +0200

    + wallpapers

---
+++ i3/wallpaper.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+<<'EOF'
+l=()
+for x in "/home/gerry/.config/i3"/*; do
+    if [[ $x =~ .*\.jpg$ ]] ; then
+        l+=("$x")
+    fi;
+done;
+
+if [[ "${#l[@]}" == "0" ]] ; then
+    exit
+fi
+
+wall="${l[RANDOM%${#l[@]}]}"
+
+feh --bg-fill "$wall"
+EOF
+
+feh --bg-fill --randomize /home/gerry/.config/i3/*.jpg
